c7ae595d4495c32475445e8261d8bb4908aafc83
Taynan Breno <taynan.breno@oqvocequer.com.br>
2023-09-22 10:57:20 -0300

    feat(api): Add infra layer and the beginning of controller implementation

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,9 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url);
 		if (url.pathname.startsWith('/pageview/')) {
-			return pageViewCounter(request, env, ctx);
+			return await pageViewCounter(request, env, ctx);
+		} else if (url.pathname === '/teste') {
+			return new Response('ooiii');
 		} else {
 			return new Response('Not Found', { status: 404 });
 		}
